9c06f781817d29d78fa2eeb6be76c277f099e1fd
wlovelace <Wlovelac@purdue.edu>
2025-12-01 11:24:16 -0500

    It works... might have had to restart to get it though

--- include/hub75.h
+++ include/hub75.h
@@ -1,37 +1,55 @@
-#ifndef HUB75_H
-#define HUB75_H
+#ifndef HUB75_SIMPLE_H
+#define HUB75_SIMPLE_H
 
 #include <stdint.h>
 #include <stdbool.h>
 
-// Panel geometry (32x32 1:16 scan)
-#define HUB75_WIDTH  32
-#define HUB75_HEIGHT 32
-#define HUB75_ROWS   (HUB75_HEIGHT/2)  // 16 (addresses)
+// Panel geometry
+#define PANEL_WIDTH  32
+#define PANEL_HEIGHT 32
 
-#define HUB75_R1   8
-#define HUB75_G1   9
-#define HUB75_B1   10
-#define HUB75_R2   11
-#define HUB75_G2   12
-#define HUB75_B2   13
+// Pin definitions
+#define PIN_R1   8
+#define PIN_G1   9
+#define PIN_B1   10
+#define PIN_R2   11
+#define PIN_G2   12
+#define PIN_B2   13
+#define PIN_CLK  14
+#define PIN_LAT  15
+#define PIN_OE   16
+#define PIN_A    17
+#define PIN_B    18
+#define PIN_C    19
+#define PIN_D    20
 
-#define HUB75_CLK  14
-#define HUB75_LAT  15
-#define HUB75_OE   16
+// Simple RGB color structure
+typedef struct {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+} rgb_t;
 
-#define HUB75_A    17
-#define HUB75_B    18
-#define HUB75_C    19
-#define HUB75_D    20
+// Initialize the display
+void display_init(void);
 
-// API
-void hub75_init(void);                    // initialize PIO/DMA, allocate buffers
-void hub75_deinit(void);                  // stop everything, free resources
-void hub75_clear_backbuffer(void);        // clear back buffer to black
-uint8_t *hub75_backbuffer(void);          // pointer to back buffer (raw pixel format)
-void hub75_swap_buffers(bool copy);       // present back buffer (copy=true copies front->back after swap)
-void hub75_draw_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b); // set pixel in back buffer (r/g/b are 0 or 1)
-void hub75_refresh_display(void);
+// Set a pixel color (x, y, r, g, b) - r/g/b are 0 or 1
+void display_set_pixel(int x, int y, uint8_t r, uint8_t g, uint8_t b);
 
-#endif // HUB75_H
+// Clear entire display
+void display_clear(void);
+
+// Refresh the display (call this regularly, e.g. in your game loop)
+void display_refresh(void);
+
+// Helper functions for common colors
+static inline void display_pixel_red(int x, int y) { display_set_pixel(x, y, 1, 0, 0); }
+static inline void display_pixel_green(int x, int y) { display_set_pixel(x, y, 0, 1, 0); }
+static inline void display_pixel_blue(int x, int y) { display_set_pixel(x, y, 0, 0, 1); }
+static inline void display_pixel_white(int x, int y) { display_set_pixel(x, y, 1, 1, 1); }
+static inline void display_pixel_yellow(int x, int y) { display_set_pixel(x, y, 1, 1, 0); }
+static inline void display_pixel_cyan(int x, int y) { display_set_pixel(x, y, 0, 1, 1); }
+static inline void display_pixel_magenta(int x, int y) { display_set_pixel(x, y, 1, 0, 1); }
+static inline void display_pixel_off(int x, int y) { display_set_pixel(x, y, 0, 0, 0); }
+
+#endif // HUB75_SIMPLE_H
--- src/main.c
+++ src/main.c
@@ -1,157 +1,66 @@
-#include <stdio.h>
-#include <string.h>
-#include "pico/stdlib.h"
-#include "hardware/timer.h"
-#include "hardware/irq.h"
-#include "music.h"
-#include "joystick.h"
+// #include <stdio.h>
+// #include <string.h>
+// #include "pico/stdlib.h"
+// #include "hardware/timer.h"
+// #include "hardware/irq.h"
+// #include "music.h"
+// #include "joystick.h"
 
-//////////////////////////////////////////////////////////////////////////////
-
-
-
-//////////////////////////////////////////////////////////////////////////////
-
-
-#include <stdio.h>
-#include <string.h>
-#include "pico/stdlib.h"
-#include "hub75.h"
-
-int main() {
-    stdio_init_all();
-    sleep_ms(2000); // Wait for USB serial to stabilize
-    
-    printf("Starting HUB75 test...\n");
-
-    // Initialize the HUB75 driver
-    hub75_init();
-    printf("HUB75 initialized\n");
-
-    // Test 1: Simple single pixel test
-    printf("Test 1: Single green pixel at (0,0)\n");
-    hub75_clear_backbuffer();
-    hub75_draw_pixel(0, 0, 0, 1, 0);  // Top-left corner green
-    hub75_swap_buffers(false);
-    sleep_ms(2000);
-
-    // Test 2: Four corner pixels
-    printf("Test 2: Four corner pixels (red)\n");
-    hub75_clear_backbuffer();
-    hub75_draw_pixel(0, 0, 1, 0, 0);              // Top-left red
-    hub75_draw_pixel(31, 0, 1, 0, 0);             // Top-right red
-    hub75_draw_pixel(0, 31, 1, 0, 0);             // Bottom-left red
-    hub75_draw_pixel(31, 31, 1, 0, 0);            // Bottom-right red
-    hub75_swap_buffers(false);
-    sleep_ms(2000);
-
-    // Test 3: Full screen red
-    printf("Test 3: Full screen red\n");
-    hub75_clear_backbuffer();
-    for (int y = 0; y < HUB75_HEIGHT; y++) {
-        for (int x = 0; x < HUB75_WIDTH; x++) {
-            hub75_draw_pixel(x, y, 1, 0, 0);
-        }
-    }
-    hub75_swap_buffers(false);
-    sleep_ms(2000);
-
-    // Test 4: Full screen green
-    printf("Test 4: Full screen green\n");
-    hub75_clear_backbuffer();
-    for (int y = 0; y < HUB75_HEIGHT; y++) {
-        for (int x = 0; x < HUB75_WIDTH; x++) {
-            hub75_draw_pixel(x, y, 0, 1, 0);
-        }
-    }
-    hub75_swap_buffers(false);
-    sleep_ms(2000);
-
-    // Test 5: Full screen blue
-    printf("Test 5: Full screen blue\n");
-    hub75_clear_backbuffer();
-    for (int y = 0; y < HUB75_HEIGHT; y++) {
-        for (int x = 0; x < HUB75_WIDTH; x++) {
-            hub75_draw_pixel(x, y, 0, 0, 1);
-        }
-    }
-    hub75_swap_buffers(false);
-    sleep_ms(2000);
-
-    // Test 6: White (all colors)
-    printf("Test 6: Full screen white\n");
-    hub75_clear_backbuffer();
-    for (int y = 0; y < HUB75_HEIGHT; y++) {
-        for (int x = 0; x < HUB75_WIDTH; x++) {
-            hub75_draw_pixel(x, y, 1, 1, 1);
-        }
-    }
-    hub75_swap_buffers(false);
-    
-    printf("Test complete. Display should be white now.\n");
-    printf("Entering continuous refresh loop...\n");
-    
-    hub75_swap_buffers(false);
-    // Continuous refresh loop - keep the display alive
-    while (true) {
-        // Continuously refresh all rows
-        hub75_swap_buffers(false);
-        sleep_us(200); 
-    }
-
-    return 0;
-}
+// //////////////////////////////////////////////////////////////////////////////
 
 
 
-/*
-int main()
-{
-    joystick_init();
-    uint16_t x = 0;
-    uint16_t y = 0;
-    bool button_pressed = false;
-    int screen_state = 0; //0 is start screen, 1 is game, 2 is high score
-    matrix_init();
-    init_pwm_audio();
-    highscores_init_defaults(); //not yet included
-    while(1){
-        joystick_read(&x, &y);
-        button_read(button_pressed);
-        if(screen_state == 0)
-        {
-            //some code that accepts x, y and button press to select and returns the next screen state
-            screen_state = 1; //to be changed to the output of that function
-
-        } else if (screen_state == 1)
-        {
-            screen_state = 2; //again to be changed to some other function
-        } else if (screen_state == 2)
-        {
-            //display high scores and wait for button press to go back to start screen
-            if(button_pressed)
-            {
-                screen_state = 0;
-            }
-        }
-    }
-}
+// //////////////////////////////////////////////////////////////////////////////
 
 
-psuedocode draft for main is 
-main: //kind of a state machine
-    game start, init joystick,
-    while(1):
-        read joystick
-        if in start screen:
-            update cursor position based on joystick
-            if select pressed on start:
-                start
-            else if highscore pressed:
-                highscore
-        else if in game:
-            dead = update_snake(joystickx, joysticky) //function will have to know board state and return if player lost or not
-        else if dead:
-            print score, print if high score //can also just print leaderboard 
-            wait for button to go back to start screen
-*/
+// /*
+// int main()
+// {
+//     joystick_init();
+//     uint16_t x = 0;
+//     uint16_t y = 0;
+//     bool button_pressed = false;
+//     int screen_state = 0; //0 is start screen, 1 is game, 2 is high score
+//     matrix_init();
+//     init_pwm_audio();
+//     highscores_init_defaults(); //not yet included
+//     while(1){
+//         joystick_read(&x, &y);
+//         button_read(button_pressed);
+//         if(screen_state == 0)
+//         {
+//             //some code that accepts x, y and button press to select and returns the next screen state
+//             screen_state = 1; //to be changed to the output of that function
+
+//         } else if (screen_state == 1)
+//         {
+//             screen_state = 2; //again to be changed to some other function
+//         } else if (screen_state == 2)
+//         {
+//             //display high scores and wait for button press to go back to start screen
+//             if(button_pressed)
+//             {
+//                 screen_state = 0;
+//             }
+//         }
+//     }
+// }
+
+
+// psuedocode draft for main is 
+// main: //kind of a state machine
+//     game start, init joystick,
+//     while(1):
+//         read joystick
+//         if in start screen:
+//             update cursor position based on joystick
+//             if select pressed on start:
+//                 start
+//             else if highscore pressed:
+//                 highscore
+//         else if in game:
+//             dead = update_snake(joystickx, joysticky) //function will have to know board state and return if player lost or not
+//         else if dead:
+//             print score, print if high score //can also just print leaderboard 
+//             wait for button to go back to start screen
+// */
